54c5e784392e3073ed045238629e16bf79e3c35a
zhangguangpeng <zhangguangpemng503@126.com>
2021-04-15 11:28:00 +0800

    删除下拉刷新

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -12,7 +12,6 @@ import 'package:flutter_module/page/supplier_page.dart';
 import 'package:flutter_module/page/user_agreement_page.dart';
 import 'package:flutter_module/utils/common_util.dart';
 import 'package:flutter_module/utils/sharepreferences_utils.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 
 //void main() => runApp(
@@ -48,39 +47,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return RefreshConfiguration(
-        headerBuilder: () => WaterDropHeader(),        // 配置默认头部指示器,假如你每个页面的头部指示器都一样的话,你需要设置这个
-        footerBuilder:  () => ClassicFooter(),        // 配置默认底部指示器
-        headerTriggerDistance: 80.0,        // 头部触发刷新的越界距离
-        springDescription:SpringDescription(stiffness: 170, damping: 16, mass: 1.9),         // 自定义回弹动画,三个属性值意义请查询flutter api
-        maxOverScrollExtent :100, //头部最大可以拖动的范围,如果发生冲出视图范围区域,请设置这个属性
-        maxUnderScrollExtent:0, // 底部最大可以拖动的范围
-        enableScrollWhenRefreshCompleted: true, //这个属性不兼容PageView和TabBarView,如果你特别需要TabBarView左右滑动,你需要把它设置为true
-        enableLoadingWhenFailed : true, //在加载失败的状态下,用户仍然可以通过手势上拉来触发加载更多
-        hideFooterWhenNotFull: false, // Viewport不满一屏时,禁用上拉加载更多功能
-        enableBallisticLoad: true, // 可以通过惯性滑动触发加载更多
-        child: MaterialApp(
-          title: 'Opay',
-          debugShowCheckedModeBanner: false,//去除debug水印
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: (
-              token.toString() != null ? HomePage() : LoginPage()
-          ),
-          routes: {
-            "home_page":(context)=> HomePage(),
-            "recharge_record_page":(context) => RechargeRecordPage(),
-            "supplier_page":(context) => SupplierPage(),
-            "bill_details_page":(context) => BillDetailsPage(),
-            "recharge_detail_page":(context) => RechargeDetailPage(),
-            "my_page":(context) => MyPage(),
-            "login_page":(context) => LoginPage(),
-            "register_page":(context) => RegisterPage(),
-            "user_agreement":(context) => UserAgreementPage()
-          },
-          builder: EasyLoading.init(),
-        )
+    return MaterialApp(
+      title: 'Opay',
+      debugShowCheckedModeBanner: false,//去除debug水印
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: (
+          token.toString() != null ? HomePage() : LoginPage()
+      ),
+      routes: {
+        "home_page":(context)=> HomePage(),
+        "recharge_record_page":(context) => RechargeRecordPage(),
+        "supplier_page":(context) => SupplierPage(),
+        "bill_details_page":(context) => BillDetailsPage(),
+        "recharge_detail_page":(context) => RechargeDetailPage(),
+        "my_page":(context) => MyPage(),
+        "login_page":(context) => LoginPage(),
+        "register_page":(context) => RegisterPage(),
+        "user_agreement":(context) => UserAgreementPage()
+      },
+      builder: EasyLoading.init(),
     );
   }
 }
